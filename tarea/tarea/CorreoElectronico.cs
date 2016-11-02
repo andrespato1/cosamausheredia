bf0a14e5dfc20dc769f1ce74c5ce2886fa903cfb
jorge andres mora <andrespato1@gmail.com>
2016-11-01 21:30:15 -0600

    se agrega el contenido de la clase factura pdf la misma para realizar sus funciones hace unso de una libreria no incluida en visual los ddl usasdos pertenecen a MigraDoc PdfSharp
    att: indiana biassetty

--- tarea/tarea/CorreoElectronico.cs
+++ tarea/tarea/CorreoElectronico.cs
@@ -10,7 +10,6 @@ using System.Windows.Forms;
 
 namespace tarea
 {
-    //prueba
     class CorreoElectronico
     {
         /*
--- tarea/tarea/FacturaPDF.cs
+++ tarea/tarea/FacturaPDF.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
 
 namespace tarea
 {
     class FacturaPDF
     {
+        //Atributo el cual extrae la fecha del sistema 
+        DateTime thisDay = DateTime.Today;
+        //Atributos para generar PDF 
+        public void crearFacturaPDF()
+        {
+            PdfDocument pdf = new PdfDocument();
+            PdfPage pdfPage = pdf.AddPage();
+            XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+            XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+
+            graph.DrawString("This is my first PDF document  ", font, XBrushes.Black,
+            new XRect(0, 0, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+            graph.DrawString("This is my second PDF document ", font, XBrushes.Black,
+            new XRect(0, 40, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+            graph.DrawString("This is my last PDF document", font, XBrushes.Black,
+            new XRect(0, 80, pdfPage.Width.Point, pdfPage.Height.Point), XStringFormats.TopLeft);
+            pdf.Save("C:\\Users\\andres\\Desktop\\prueba.pdf");
+        }
     }
 }
