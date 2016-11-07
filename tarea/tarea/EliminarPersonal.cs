4aaf43f0ce14623098c2a7bb49141ab0c425a9af
jorge andres mora <andrespato1@gmail.com>
2016-11-07 08:23:32 -0600

    eliminar personal

--- tarea/tarea/EliminarPersonal.cs
+++ tarea/tarea/EliminarPersonal.cs
@@ -12,9 +12,42 @@ namespace tarea
 {
     public partial class EliminarPersonal : Form
     {
+        ConexionSQLVisualStudio baseDatos = new ConexionSQLVisualStudio();
+        string nombreTabla = "personal";
         public EliminarPersonal()
         {
             InitializeComponent();
+            baseDatos.seleccionarValoresBaseDatos(DataGridEliminarPersonal, nombreTabla, "*", "");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtIDEliminar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEliminarPersonal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAtras_Click(object sender, EventArgs e)
+        {
+            string queryActualizar = "update personal set puestoTrab = null";
+            baseDatos.actualizarValoresBaseDatos(queryActualizar, "IDPersonal", TxtIDEliminar.Text);
+            queryActualizar = "update personal set fechaRetiro = '" + TxtFechaRetiro.Text+ "'";
+            baseDatos.actualizarValoresBaseDatos(queryActualizar, "IDPersonal", TxtIDEliminar.Text);
+            queryActualizar = "update personal set IDJefe = " + TxtEncargado.Text;
+            baseDatos.actualizarValoresBaseDatos(queryActualizar, "IDJefe", TxtIDEliminar.Text);
+
+            baseDatos.seleccionarValoresBaseDatos(DataGridEliminarPersonal, nombreTabla, "*", "");
+            TxtEncargado.Text = "";
+            TxtIDEliminar.Text = "";
+            TxtFechaRetiro.Text = "";
         }
     }
 }
