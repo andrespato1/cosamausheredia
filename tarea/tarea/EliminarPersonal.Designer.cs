4652aa878c49debd2a1c7656347cd742c9395f5d
jorge andres mora <andrespato1@gmail.com>
2016-11-07 07:02:25 -0600

    se crea ventana que elimina el personal

---
+++ tarea/tarea/EliminarPersonal.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace tarea
+{
+    partial class EliminarPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // EliminarPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 300);
+            this.Name = "EliminarPersonal";
+            this.Text = "EliminarPersonal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
