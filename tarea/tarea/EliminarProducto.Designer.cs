ffc10a51aa6d3ecd3629eb8288c93b20ab37a471
jorge andres mora <andrespato1@gmail.com>
2016-11-06 21:47:26 -0600

    ventana eliminar producto

---
+++ tarea/tarea/EliminarProducto.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace tarea
+{
+    partial class EliminarProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnEliminarProducto = new System.Windows.Forms.Button();
+            this.TxtEliminarProducto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnEliminarProducto
+            // 
+            this.BtnEliminarProducto.Location = new System.Drawing.Point(335, 12);
+            this.BtnEliminarProducto.Name = "BtnEliminarProducto";
+            this.BtnEliminarProducto.Size = new System.Drawing.Size(75, 23);
+            this.BtnEliminarProducto.TabIndex = 0;
+            this.BtnEliminarProducto.Text = "Eliminar";
+            this.BtnEliminarProducto.UseVisualStyleBackColor = true;
+            // 
+            // TxtEliminarProducto
+            // 
+            this.TxtEliminarProducto.Location = new System.Drawing.Point(215, 14);
+            this.TxtEliminarProducto.Name = "TxtEliminarProducto";
+            this.TxtEliminarProducto.Size = new System.Drawing.Size(100, 20);
+            this.TxtEliminarProducto.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese el SKU del producto a eliminar";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(577, 196);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // EliminarProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtEliminarProducto);
+            this.Controls.Add(this.BtnEliminarProducto);
+            this.Name = "EliminarProducto";
+            this.Text = "EliminarProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEliminarProducto;
+        private System.Windows.Forms.TextBox TxtEliminarProducto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
